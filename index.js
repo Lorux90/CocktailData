442431d488cdce0d5d5daaba02413a66966d728d
Massimo Gaudiano <maximo73@libero.it>
2020-07-06 11:01:00 +0000

    aggiunto axios su random

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 let express = require("express");
 let path = require("path");
 let app = express();
+
 app.use(express.static("assets"));
+
 app.get("/", (req, res) => {
   res.redirect("/home");
 });
 app.get("/home", (req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
+});
+app.get("/random", (req, res) => {
+  res.sendFile(path.join(__dirname, "/random.html"));
 });
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "/404.html"));
