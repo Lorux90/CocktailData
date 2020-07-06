626cbf8cfd148b4b922083d41f440e3208adda63
Lorenzo Stievano <lorux_stie@hotmail.it>
2020-07-06 09:09:47 +0000

    endpoint interni

---
+++ index.js
@@ -0,0 +1,15 @@
+let express = require("express");
+let path = require("path");
+let app = express();
+app.use(express.static("assets"));
+app.get("/", (req, res) => {
+  res.redirect("/home");
+});
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/404.html"));
+});
+
+app.listen(3000, () => console.log("in ascolto sulla porta 3000"));
