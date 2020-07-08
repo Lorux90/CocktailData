cec34c3ce3c37186600a5e9acc7a1ecb7ec10938
Massimo Gaudiano <maximo73@libero.it>
2020-07-08 13:49:35 +0000

    aggiunto file details e mix@home

--- index.js
+++ index.js
@@ -13,6 +13,17 @@ app.get("/home", (req, res) => {
 app.get("/random", (req, res) => {
   res.sendFile(path.join(__dirname, "/random.html"));
 });
+app.get("/details", (req, res) => {
+  res.sendFile(path.join(__dirname, "/details.html"));
+});
+app.get("/mix@home", (req, res) => {
+  res.sendFile(path.join(__dirname, "/mix@home.html"));
+});
+// app.get("/details/:id_cocktail", (req, res) => {
+//   let cocktail = req.params.id_cocktail
+//   // res.send('cocktail id ' + cocktail)
+//   res.sendFile(path.join(__dirname, "/details.html" + cocktail));
+// });
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "/404.html"));
 });
